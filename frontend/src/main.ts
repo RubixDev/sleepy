2bd76a071f273f1fb233c8876acaf32005710267
RubixDev <groh@rubixdev.de>
2025-10-06 16:08:06 +0200

    chore(frontend): include roboto font

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/material-icons';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
